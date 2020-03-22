a8e61455226a0c40302bb2cb3d28ab24dd571cfb
linfan003 <linfan0022@gmail.com>
2020-03-22 17:43:50 +0800

    leetcode

---
+++ leetcode/src/num1_100/e8IsPalindrome.java
@@ -0,0 +1,16 @@
+package num1_100;
+
+public class e8IsPalindrome {
+    public static void main(String[] args) {
+        e8IsPalindrome e8IsPalindrome = new e8IsPalindrome();
+        int x = 54321354;
+        boolean palindrome = e8IsPalindrome.isPalindrome1(x);
+        System.out.println(palindrome);
+    }
+
+
+
+    public boolean isPalindrome1(int x) {
+        x
+    }
+}
