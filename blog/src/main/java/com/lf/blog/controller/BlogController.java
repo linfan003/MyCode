5966fbcbe16697165804b51be3148fa649f24b31
linfan003 <linfan0022@gmail.com>
2020-12-09 18:03:27 +0800

    blog

---
+++ blog/src/main/java/com/lf/blog/controller/BlogController.java
@@ -0,0 +1,15 @@
+package com.lf.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @Author linfan
+ * @Date 2020/12/9 15:22
+ * @Version 1.0
+ */
+@RestController
+public class BlogController {
+
+}
